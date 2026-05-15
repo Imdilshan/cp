bdc434b41e4ccfc1b1cc89205486fe6afdd51872
Imdilshan <imdilshan@proton.me>
2026-05-16 05:01:12 +0530

    first commit

---
+++ atcoder/abc 458/a.cpp
@@ -0,0 +1,32 @@
+/* Author -> highfive */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* To Make I/O faster */
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+/* Macro */
+#define ll long long
+#define ld long double
+#define pb push_back
+#define endl '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+
+/* Containers */
+typedef pair<int,int> pi;
+typedef map<int,int> mpi;
+typedef priority_queue<int> mxpq;
+typedef priority_queue<int, vector<int>, greater<int>> mnpq;
+
+const int N = 1e5;
+
+void solve(){
+    cout << "Hello World" << endl;
+}
+
+signed main(){
+  fastio;
+  solve();
+  return 0;
+}
